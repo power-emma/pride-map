3083c470c936ea05c409eb5e3d94de1aba22e723
Lukecb14 <114123793+Lukecb14@users.noreply.github.com>
2026-02-07 16:26:26 -0500

    improved the header

--- client/pridemap/src/components/header.tsx
+++ client/pridemap/src/components/header.tsx
@@ -1,15 +1,11 @@
-import '../App.css';
+import React from 'react';
 
-const Header = () => {
+export const Header: React.FC = () => {
     return (
-        <header className="header">
-            <div className="header-left">
-                <img src="/src/assets/logo.png" alt="Pride Map Logo" className="logo" />
-                <h1>Welcome to Pride Map</h1>
-            </div>
-            <button className="header-button">Menu</button>
+        <header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '1rem' }}>
+            <img src="/src/assets/tempLogo.jpg" alt="Temporary Logo" style={{ height: '1rem' }} />
+            <h1 style={{ margin: '1em' }}>Welcome to Pride Map</h1>
+            <button>Menu</button>
         </header>
     );
 };
-
-export default Header;
