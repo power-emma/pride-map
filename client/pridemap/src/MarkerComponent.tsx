8d74be4e4c4bc40fca7bc18e1f6f23565d47c6ed
Emma Power <power.emma@outlook.com>
2026-03-03 16:59:31 -0500

    Rainbow Pin to fix Default Pin not Loading
    Rainbow pin image made by me :)

--- client/pridemap/src/MarkerComponent.tsx
+++ client/pridemap/src/MarkerComponent.tsx
@@ -1,15 +1,19 @@
 import { Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+import rainbowPin from "./assets/rainbow-pin.svg";
+
+const rainbowIcon = L.icon({
+    iconUrl: rainbowPin,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],   // tip of the pin
+    popupAnchor: [1, -34],  // popup opens above the pin
+});
 
 const MarkerComponent = ({ name, position }: { name: string, position: [number, number] }) => {
-
     return (
-        <div>
-            <Marker position={position}>
-                <Popup>
-                    {name}
-                </Popup>
-            </Marker>
-        </div>
+        <Marker position={position} icon={rainbowIcon}>
+            <Popup>{name}</Popup>
+        </Marker>
     );
 }
 
