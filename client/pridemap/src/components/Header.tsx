3f4c141de6e018a4c81db75a201096fd3aaa4a04
Emma Power <power.emma@outlook.com>
2026-03-06 18:41:31 -0500

    Dark Mode in Map

--- client/pridemap/src/components/Header.tsx
+++ client/pridemap/src/components/Header.tsx
@@ -3,9 +3,9 @@ import rainbowPin from '../assets/rainbow-pin.svg';
 
 export const Header: React.FC = () => {
     return (
-        <header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '1rem' }}>
+        <header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '0.5rem' }}>
             <img src={rainbowPin} alt="Pride Map Logo" style={{ height: '3em' }} />
-            <h1 style={{ margin: '1em' }}>Welcome to Pride Map</h1>
+            <h1 style={{  }}>Welcome to Pride Map</h1>
             <button>Menu</button>
         </header>
     );
